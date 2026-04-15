019934c41f183744b02bbb290caf31e38d879d66
LeahBasson <leahbasson54@gmail.com>
2026-04-15 11:35:46 +0200

    :pushpin:

--- GUICalculator/src/za/ac/cput/GUI.java
+++ GUICalculator/src/za/ac/cput/GUI.java
@@ -9,11 +9,15 @@ public class GUI extends JFrame implements ActionListener {
     // menus
     private JMenuBar mnuBar;
     private JMenu mnuOperation;
+    private JMenu mnuExit;
+    private JMenu mnuClear;
     // you do this so that its easy to access later for event handling.
     private JMenuItem mniAdd;
     private JMenuItem mniSubtract;
     private JMenuItem mniMultiply;
     private JMenuItem mniDivide;
+    private JMenuItem mniExit;
+    private JMenuItem mniClear;
 
     // labels
     private JLabel lblNumOne;
@@ -30,7 +34,6 @@ public class GUI extends JFrame implements ActionListener {
     private JButton btnSubtract;
     private JButton btnMultiply;
     private JButton btnDivide;
-    private JButton btnClear;
 
     // panels
     private JPanel pnlMenu;
@@ -66,24 +69,32 @@ public class GUI extends JFrame implements ActionListener {
         btnSubtract = new JButton("Subtract");
         btnMultiply = new JButton("Multiply");
         btnDivide = new JButton("Divide");
-        btnClear = new JButton("Clear");
 
         // menu
         mnuBar = new JMenuBar();
+        // sets the bar onto the frame
+        setJMenuBar(mnuBar);
         mnuOperation = new JMenu(("Operation"));
+        mnuClear = new JMenu("Clear");
+        mnuExit = new JMenu("Exit");
         mniAdd = new JMenuItem("Add");
         mniSubtract = new JMenuItem("Subtract");
         mniMultiply = new JMenuItem("Multiply");
         mniDivide = new JMenuItem("Divide");
+        mniExit = new JMenuItem("Exit");
+        mniClear = new JMenuItem("Clear");
 
         mnuOperation.add(mniAdd);
         mnuOperation.add(mniSubtract);
         mnuOperation.add(mniMultiply);
         mnuOperation.add(mniDivide);
+        mnuClear.add(mniClear);
+        mnuExit.add(mniExit);
 
         mnuBar.add(mnuOperation);
-        pnlMenu.add(mnuBar);
-        pnlMenu.add(btnClear);
+        mnuBar.add(mnuClear);
+        mnuBar.add(mnuExit);
+        //pnlMenu.add(mnuBar);
         add(pnlMenu);
 
         pnlForm.add(lblNumOne);
@@ -106,11 +117,12 @@ public class GUI extends JFrame implements ActionListener {
         btnSubtract.addActionListener(this);
         btnMultiply.addActionListener(this);
         btnDivide.addActionListener(this);
-        btnClear.addActionListener(this);
+        mniClear.addActionListener(this);
         mniAdd.addActionListener(this);
         mniSubtract.addActionListener(this);
         mniMultiply.addActionListener(this);
         mniDivide.addActionListener(this);
+        mniExit.addActionListener(this);
 
     }
 
@@ -129,11 +141,57 @@ public class GUI extends JFrame implements ActionListener {
             }
         }
 
+        if (e.getSource() == btnSubtract || e.getSource() == mniSubtract) {
+            try {
+                double num1 = Double.parseDouble(txtNum1.getText());
+                double num2 = Double.parseDouble(txtNum2.getText());
+                double result = num1 - num2;
+                txtResult.setText(Double.toString(result));
+
+            } catch (NumberFormatException a) {
+                JOptionPane.showMessageDialog(null, "Please enter valid numbers");
+            }
+        }
+
+        if (e.getSource() == btnMultiply || e.getSource() == mniMultiply) {
+            try {
+                double num1 = Double.parseDouble(txtNum1.getText());
+                double num2 = Double.parseDouble(txtNum2.getText());
+                double result = num1 * num2;
+                txtResult.setText(Double.toString(result));
+
+            } catch (NumberFormatException a) {
+                JOptionPane.showMessageDialog(null, "Please enter valid numbers");
+            }
+        }
+
+        if (e.getSource() == btnDivide || e.getSource() == mniDivide) {
+            try {
+                int num1 = Integer.parseInt(txtNum1.getText());
+                int num2 = Integer.parseInt(txtNum2.getText());
+                double result = num1 / num2;
+                txtResult.setText(Double.toString(result));
+
+            } catch (ArithmeticException a) {
+                JOptionPane.showMessageDialog(null, "Cannot divide by zero");
+            } catch (NumberFormatException a) {
+                JOptionPane.showMessageDialog(null, "Please enter valid numbers");
+            }
+        }
+
         // doing it this way so that the text doesnt immediately clear after
-        if (e.getSource() == btnClear) {
-            txtNum1.setText("");
-            txtNum2.setText("");
-            txtResult.setText("");
+        if (e.getSource() == mniClear) {
+                if (txtNum1.getText().equals("") && txtNum2.getText().equals("")){
+                    JOptionPane.showMessageDialog(null, "No values to clear");
+                }
+                txtNum1.setText("");
+                txtNum2.setText("");
+                txtResult.setText("");
+                
+        }
+
+        if (e.getSource() == mniExit) {
+            dispose();
         }
     }
 
